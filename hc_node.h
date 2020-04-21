f9905d80359b46bced935753ca4c162fe3bb5351
Alex Olson <alexkellenolson@gmail.com>
2020-04-21 22:27:18 +0200

    rename node

---
+++ hc_node.h
@@ -0,0 +1,15 @@
+#ifndef HC_NODE_H
+#define HC_NODE_H
+
+typedef struct hc_node {
+    struct hc_node* left;
+    struct hc_node* right;
+    const char* key;
+    const char* value;
+} hc_node;
+
+hc_node* hc_node_init(const char* k, const char* v);
+
+void hc_node_destroy(hc_node** n);
+
+#endif
