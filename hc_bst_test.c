a61901d11ccfa79cb23ee92525b46b64bc8aa48e
Alex Olson <alexkellenolson@gmail.com>
2020-05-01 22:46:25 +0200

    improve driver

--- hc_bst_test.c
+++ hc_bst_test.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define TEST_KEYS_SIZE 10
 
@@ -14,16 +15,29 @@ int main() {
     const char* test_keys[TEST_KEYS_SIZE] = {"m", "a", "y", "o", "z",
                                              "b", "c", "d", "e", "f"};
 
-    for (int i = 0; i < TEST_KEYS_SIZE; ++i)
+    for (int i = 0; i < TEST_KEYS_SIZE; ++i) {
+        if (strcmp(test_keys[i], "z") == 0) {
+            hc_bst_insert(tree, test_keys[i], "zebra");
+            continue;
+        }
         hc_bst_insert(tree, test_keys[i], NULL);
+    }
 
+    assert(hc_bst_get_height(tree) == 7);
+    assert(hc_bst_get(tree, "z") == "zebra");
+
+    printf("\n\n================ Pre-Order Traversal ================\n");
     hc_bst_traverse(tree, -1);
+    printf("\n\n================ In-Order Traversal ================\n");
     hc_bst_traverse(tree, 0);
+    printf("\n\n================ Post-Order Traversal ================\n");
     hc_bst_traverse(tree, 1);
+    printf("\n\n================ Level-Order Traversal ================\n");
     hc_bst_traverse(tree, 2);
 
     hc_bst_delete_key(tree, "y");
 
+    printf("\n\n================ Final State of Tree ================\n");
     hc_bst_print(tree);
 
     hc_bst_destroy(&tree);
